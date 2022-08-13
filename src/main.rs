55a10cbb3d28e5ccb58e8b5bbc33fd02bbb9f165
Sam Segal <samsegal@sams-mbp.lan>
2022-08-13 16:11:54 -0600

    TODO add file read/Refactor

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+
+fn main() {
+    let config : Vec<String> = env::args().collect();
+    let config = Config::new(&config).unwrap();
+    println!("key: {}",config.key);
+    println!("file: {}",config.file);
+    println!("argument: {}",config.argument);
+
+}
+
+struct Config {
+    key: String,
+    file: String,
+    argument: String,
+}
+
+impl Config {
+    fn new (args : &Vec<String>) -> Result<Config,&'static str> {
+        if args.len() < 4 {
+            return Err("not enough arguments")
+        }
+        
+        let key = args[1].clone();
+        let file = args[2].clone();
+        let argument = args[3].clone();
+        Ok(Config{key,file,argument})
+    }    
+}
